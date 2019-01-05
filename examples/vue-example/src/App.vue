f80418f2f9672d6b6819ea611c420b398ac8f760
Tamas Szoke <tamas.szoke.dev@gmail.com>
2019-01-05 16:03:59 +0100

    [ADD]: Vue example

---
+++ examples/vue-example/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <img @click="clickSound" src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import sfx from 'simple-sfx'
+// eslint-disable-next-line
+this.audio = new sfx([
+  './static/start.ogg'
+], true)
+
+export default {
+  name: 'App',
+  methods: {
+    clickSound: () => {
+      this.audio.play('start')
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ examples/vue-example/src/components/HelloWorld.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="hello">
+    <h2>Click on the logo to test the sound!</h2>
+    <p>Watch the console for more.</p>
+    <a
+      className="App-link"
+      href="https://github.com/tamasszoke/simple-sfx"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      simple-sfx module
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
