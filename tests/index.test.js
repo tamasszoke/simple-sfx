43b150a20e7eb0b8cb0d8ec92a5bc72293a4214c
Tamas Szoke <tamas.szoke.dev@gmail.com>
2019-01-06 09:50:11 +0100

    [UPDATE]: Test functions

--- tests/index.test.js
+++ tests/index.test.js
@@ -3,18 +3,18 @@ import ReactSfx from '../src/index.js';
 
 describe('ReactSfx Component', () => {
 
-  window.HTMLMediaElement.prototype.load = () => { /* do nothing */ };
-  window.HTMLMediaElement.prototype.play = () => { /* do nothing */ };
-  window.HTMLMediaElement.prototype.pause = () => { /* do nothing */ };
+  window.HTMLMediaElement.prototype.load = () => { /* Do nothing */ };
+  window.HTMLMediaElement.prototype.play = () => { /* Do nothing */ };
+  window.HTMLMediaElement.prototype.pause = () => { /* Do nothing */ };
 
-  it('should load audio component', function() {
-    let audio;
+  it('should load audio component', () => {
+    let audio = null;
     expect(() => { audio = new ReactSfx(); }).to.throw(Error) &&
     expect(() => { audio = new ReactSfx(['/example/public/click.mp3'], false); }).to.not.throw(Error) &&
     expect(audio).to.be.an('object');
   })
 
-  it('should play audio, string parameter', function() {
+  it('should play audio, string parameter', () => {
     let audio = new ReactSfx([
       '/example/public/click.mp3'
     ], false);
@@ -23,7 +23,7 @@ describe('ReactSfx Component', () => {
     expect(() => { audio.play('click'); }).to.not.throw(Error);
   })
 
-  it('should play audio, object parameter', function() {
+  it('should play audio, object parameter', () => {
     let audio = new ReactSfx([
       '/example/public/click.mp3'
     ], false);
@@ -33,7 +33,7 @@ describe('ReactSfx Component', () => {
     expect(() => { audio.play({ name: 'click', volume: .5, loop: true }); }).to.not.throw(Error);
   })
   
-  it('should stop audio, string parameter', function() {
+  it('should stop audio, string parameter', () => {
     let audio = new ReactSfx([
       '/example/public/click.mp3'
     ], false);
@@ -41,7 +41,7 @@ describe('ReactSfx Component', () => {
     expect(() => { audio.stop('click'); }).to.not.throw(Error);
   })
   
-  it('should stop audio, array parameter', function() {
+  it('should stop audio, array parameter', () => {
     let audio = new ReactSfx([
       '/example/public/click.mp3',
       '/example/public/clack.mp3'
@@ -52,21 +52,21 @@ describe('ReactSfx Component', () => {
     expect(() => { audio.stop(['click', 'clack']); }).to.not.throw(Error);
   })
 
-  it('should stop all audio', function() {
+  it('should stop all audio', () => {
     let audio = new ReactSfx([
       '/example/public/click.mp3'
     ], false);
     expect(() => { audio.stop(); }).to.not.throw(Error);
   })
 
-  it('should switch on audio', function() {
+  it('should switch on audio', () => {
     let audio = new ReactSfx([
       '/example/public/click.mp3'
     ], false);
     expect(() => { audio.on(); }).to.not.throw(Error);
   })
 
-  it('should switch off audio', function() {
+  it('should switch off audio', () => {
     let audio = new ReactSfx([
       '/example/public/click.mp3'
     ], false);
