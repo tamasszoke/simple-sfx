a7dd0a06b5ff76a1f819c9c2ba4415ecf30c4f3f
Tamas Szoke <tamas.szoke.dev@gmail.com>
2019-01-15 22:34:20 +0100

    [ADD]: ESLint standard style

--- src/index.js
+++ src/index.js
@@ -1,202 +1,201 @@
 /**
  * Main object
- * @param {array} audioList 
- * @param {bool} devMode 
+ * @param {array} audioList
+ * @param {bool} devMode
  */
 const sfx = (audioList, devMode) => {
+  /**
+   * Defaults
+   */
+  let audio = []
+  let audioOn = true
 
-	/**
-	 * Defaults
-	 */
-	let audio = [];
-	let audioOn = true;
-	
-	/**
-	 * Pre-load list of audio files
-	 * @param {array} audioList
-	 */
-	const load = (audioList) => {
-		let audioName;
-		if (audioList && audioList.length > 0) {
-			try {
-				for (let i = 0; i < audioList.length; i++) {
-					audioName = audioList[i].replace(/^.*[\\\/]/, ''); // remove path
-          audioName = audioName.slice(0, -4); // remove extension
+  /**
+   * Pre-load list of audio files
+   * @param {array} audioList
+   */
+  const load = (audioList) => {
+    let audioName
+    if (audioList && audioList.length > 0) {
+      try {
+        for (let i = 0; i < audioList.length; i++) {
+          // eslint-disable-next-line
+          audioName = audioList[i].replace(/^.*[\\\/]/, '') // remove path
+          audioName = audioName.slice(0, -4) // remove extension
           let data = new Audio(audioList[i]) // with path and extension
           data.onerror = () => {
-            throw new Error('File not found: ' + audioList[i]);
-          };
-					audio.push({
-						name: audioName, // file name only, without extension
-						data: data
-          });
-					devMode ? console.log('Audio loaded: ' + audioName) : null;
-				};
-				devMode ? console.log('All audio loaded, total items: ' + audio.length) : null;
-			} catch(error) {
-				throw new Error('Pre-loading! ' + error);
-			};
-		} else {
-			throw new Error('Parameter not found!');
-		};
-	}
+            throw new Error('File not found: ' + audioList[i])
+          }
+          audio.push({
+            name: audioName, // file name only, without extension
+            data: data
+          })
+          devMode && console.log('Audio loaded: ' + audioName)
+        }
+        devMode && console.log('All audio loaded, total items: ' + audio.length)
+      } catch (error) {
+        throw new Error('Pre-loading! ' + error)
+      }
+    } else {
+      throw new Error('Parameter not found!')
+    }
+  }
 
-	/**
-	 * Play audio by name
-	 * @param {string/object} settings 
-	 */
-	const play = (settings) => {
+  /**
+   * Play audio by name
+   * @param {string/object} settings
+   */
+  const play = (settings) => {
     if (settings) {
       if (audioOn === true) {
         if (typeof settings === 'string') {
           settings = {
-						name: settings,
-						volume: 1
+            name: settings,
+            volume: 1
           }
-				}
-				if (typeof settings === 'object' && Object.keys(settings).length > 0) {
-					for (let i = 0; i < audio.length; i++) {
-						if (audio[i].name === settings.name) {
-							if (settings.loop) { // loop playing
-								try {
-									audio[i].data.addEventListener('ended', () => {
-										stop(settings.name);
-										play({
-											name: settings.name,
-											loop: false, // got eventlistener, so it will always play if ended !
-											volume: settings.volume || 1
-										});
-									});
-									audio[i].data.volume = settings.volume || 1;
-									audio[i].data.play();
-                  devMode ? console.log('Playing audio: ' + settings.name) : null;
-                  return 2;
-								} catch (error) {
-									throw new Error('Loop playing audio! ' + error);
-								};
-							} else { // normal playing
-								try {
-									audio[i].data.volume = settings.volume || 1;
-									audio[i].data.pause();
-									audio[i].data.currentTime = 0;
-									setTimeout(() => { // prevent pause-play collision
-										audio[i].data.play();
-                    devMode ? console.log('Playing audio: ' + settings.name) : null;
-                  }, 1);
-                  return 1;
-								} catch (error) {
-									throw new Error('Normal playing audio! ' + error);
-								};
-							};
-							return;
-						};
-					};
-					throw new Error('Audio not found! ' + settings.name);
-				} else {
-					throw new Error('Bad parameters!');
-				};
+        }
+        if (typeof settings === 'object' && Object.keys(settings).length > 0) {
+          for (let i = 0; i < audio.length; i++) {
+            if (audio[i].name === settings.name) {
+              if (settings.loop) { // loop playing
+                try {
+                  audio[i].data.addEventListener('ended', () => {
+                    stop(settings.name)
+                    play({
+                      name: settings.name,
+                      loop: false, // got eventlistener, so it will always play if ended !
+                      volume: settings.volume || 1
+                    })
+                  })
+                  audio[i].data.volume = settings.volume || 1
+                  audio[i].data.play()
+                  devMode && console.log('Playing audio: ' + settings.name)
+                  return 2
+                } catch (error) {
+                  throw new Error('Loop playing audio! ' + error)
+                };
+              } else { // normal playing
+                try {
+                  audio[i].data.volume = settings.volume || 1
+                  audio[i].data.pause()
+                  audio[i].data.currentTime = 0
+                  setTimeout(() => { // prevent pause-play collision
+                    audio[i].data.play()
+                    devMode && console.log('Playing audio: ' + settings.name)
+                  }, 1)
+                  return 1
+                } catch (error) {
+                  throw new Error('Normal playing audio! ' + error)
+                }
+              }
+            }
+          }
+          throw new Error('Audio not found! ' + settings.name)
+        } else {
+          throw new Error('Bad parameters!')
+        }
       } else {
-        devMode ? console.log('Audio disabled, not playing! ' + settings.name) : null;
-      };
+        devMode && console.log('Audio disabled, not playing! ' + settings.name)
+      }
     } else {
-			throw new Error('Parameters not found!');
-    };
-	}
+      throw new Error('Parameters not found!')
+    }
+  }
 
-	/**
-	 * Stop audio by name
-	 * @param {string/array} audioNames 
-	 */
-	const stop = (audioNames) => {
-		if (audioOn === true) {
-			if (audioNames) {
-				if (audioNames.length > 0) {
-					try {
-						if (typeof audioNames === 'string') {
-							audioNames = [ audioNames ]
-						}
-						for (let i = 0; i < audioNames.length; i++) {
-							for (let j = 0; j < audio.length; j++) {
-								if (audio[j].name === audioNames[i] && !audio[j].data.paused) {
-									audio[j].data.pause();
-									audio[j].data.currentTime = 0;
-									devMode ? console.log('Audio stopped: ' + audioNames[i]) : null;
-									return 1;
-								} else if (audio[j].name === audioNames[i] && audio[j].data.paused) {
-									return 1;
-								};
-							};
-						};
-					} catch(error) {
-						throw new Error('Stopping audio! ' + error);
-					};
-					throw new Error('Audio not found! ' + audioNames);
-				} else {
-					throw new Error('Bad parameter!');
-				}
-			} else {
-        stopAll();
-        return 2;
-			};
-		};
-	}
+  /**
+   * Stop audio by name
+   * @param {string/array} audioNames
+   */
+  const stop = (audioNames) => {
+    if (audioOn === true) {
+      if (audioNames) {
+        if (audioNames.length > 0) {
+          try {
+            if (typeof audioNames === 'string') {
+              audioNames = [ audioNames ]
+            }
+            for (let i = 0; i < audioNames.length; i++) {
+              for (let j = 0; j < audio.length; j++) {
+                if (audio[j].name === audioNames[i] && !audio[j].data.paused) {
+                  audio[j].data.pause()
+                  audio[j].data.currentTime = 0
+                  devMode && console.log('Audio stopped: ' + audioNames[i])
+                  return 1
+                } else if (audio[j].name === audioNames[i] && audio[j].data.paused) {
+                  return 1
+                }
+              }
+            }
+          } catch (error) {
+            throw new Error('Stopping audio! ' + error)
+          }
+          throw new Error('Audio not found! ' + audioNames)
+        } else {
+          throw new Error('Bad parameter!')
+        }
+      } else {
+        stopAll()
+        return 2
+      }
+    }
+  }
 
-	/**
-	 * Stop all audio
-	 */
-	const stopAll = () => {
-		if (audioOn === true) {
-			try {
-				for (let i = 0; i < audio.length; i++) {
-					if (!audio[i].data.paused) {
-						audio[i].data.pause();
-						audio[i].data.currentTime = 0;
-					};
-				};
-				devMode ?	console.log('All audio stopped.') : null;
-			} catch(error) {
-				throw new Error('Stopping all audio! ' + error);
-			};
-		};
-	}
+  /**
+   * Stop all audio
+   */
+  const stopAll = () => {
+    if (audioOn === true) {
+      try {
+        for (let i = 0; i < audio.length; i++) {
+          if (!audio[i].data.paused) {
+            audio[i].data.pause()
+            audio[i].data.currentTime = 0
+          }
+        }
+        devMode && console.log('All audio stopped.')
+      } catch (error) {
+        throw new Error('Stopping all audio! ' + error)
+      }
+    }
+  }
 
-	/**
-	 * Switch on audio
-	 */
-	const on = () => {
-		audioOn = true;
-    devMode ? console.log('Audio enabled.') : null;
-    return 1;
-	}
+  /**
+   * Switch on audio
+   */
+  const on = () => {
+    audioOn = true
+    devMode && console.log('Audio enabled.')
+    return 1
+  }
 
-	/**
-	 * Switch off audio
-	 */
-	const off = () => {
-		try {
-			stopAll();
-			audioOn = false;
-			devMode ? console.log('Audio disabled.') : null;
-      return 1;
-		} catch(error) {
-			throw new Error('Switching off! ' + error);
-		};
-	}
-	
-	/**
-	 * Pre-load audio
-	 */
-	load(audioList);
+  /**
+   * Switch off audio
+   */
+  const off = () => {
+    try {
+      stopAll()
+      audioOn = false
+      devMode && console.log('Audio disabled.')
+      return 1
+    } catch (error) {
+      throw new Error('Switching off! ' + error)
+    }
+  }
 
-	/**
-	 * Public methods
-	 */
-	return {
-		on,
-		off,
-		play,
-		stop
-	}
+  /**
+   * Pre-load audio
+   */
+  load(audioList)
+
+  /**
+   * Public methods
+   */
+  return {
+    on,
+    off,
+    play,
+    stop
+  }
 }
 
-export default sfx;
+export default sfx
